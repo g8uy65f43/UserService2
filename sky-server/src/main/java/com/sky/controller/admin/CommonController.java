75aca2e7cf7e4b580648fd721a0ebddd7d8e6121
【Egrowth】TIANCHENGWE <g8uy65f43@gmail.com]>
2023-12-25 18:22:32 +0900

    従業員追加コード

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,10 +1,12 @@
 package com.sky.controller.admin;
 
 import com.sky.annotation.AutoFill;
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,28 +28,43 @@ import java.util.UUID;
 public class CommonController {
 
 
-
-
+    String tempDir = System.getProperty("java.io.tmpdir");
+    String relativePath = "src/update";  // 相对于项目根目录的路径
 
     @PostMapping("upload")
     @ApiOperation("ファールアップロード")
     public Result<String> upload(MultipartFile file) throws IOException {
-log.info("ファールアップロード：｛｝",file);
-        // Generate a unique file name or use the original file name
-        String fileName =  file.getOriginalFilename();
-        String extension = fileName.substring(fileName.lastIndexOf("."));
-        String objectName = UUID.randomUUID().toString()+extension;
+        log.info("ファールアップロード：{}", file);
 
-        // Save the file to the server
-        String absolutePath = "C:\\develop\\sky-take-out\\sky-server\\src\\main\\resources\\update";
-        String filePath =absolutePath + File.separator + fileName;
-        // 打印保存路径
-          System.out.println(filePath);
-          // 保存文件的路径信息
-          File saveFile = new File(filePath);
-          // 文件保存
-        file.transferTo(saveFile);
-        return Result.success();
-     }
+        // Generate a unique file name or use the original file name
+        String fileName = file.getOriginalFilename();
+        String extension = fileName.substring(fileName.lastIndexOf("."));
+        String objectName = UUID.randomUUID().toString() + extension;
+        File directory = new File("");//设定为当前文件夹
+        try{
+            System.out.println(directory.getCanonicalPath());//获取标准的路径
+            String filePath = directory.getCanonicalPath()+"\\nginx-1.20.2\\html\\sky\\img\\upload\\" + objectName;
+            try {
+                // 构建文件保存路径
+
+                // 打印保存路径
+                System.out.println(filePath);
+
+                // 保存文件的路径信息
+                File saveFile = new File(filePath);
+
+                // 文件保存
+                file.transferTo(saveFile);
+                String avatar = "/images/"+objectName;
+                return Result.success(avatar);
+            } catch (IOException e) {
+                log.error("文件上传失败：{}", e);
+            }
+
+        }catch(Exception e){}
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
 
 }
